dffa6e1cec6a5ab46a855db8e0af268b389b55c5
Hemant <hkbiet@gmail.com>
2024-03-19 22:26:26 +0530

    refactored to use multiprocessing and speed up.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import re
 from multiprocessing import Process
@@ -7,8 +8,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from multiprocessing import Queue
 
-
+# Globals
 inputted_language = ""
+queue = Queue()
+root_url = 'https://summerofcode.withgoogle.com/'
+
+
 def find_match(user_input, result_string):
     print("Looking for : ", user_input, " in tech string : ", result_string)
     regex_pattern = r' ' + re.escape(user_input)
@@ -21,75 +26,67 @@ def find_match(user_input, result_string):
         return False
 
 
-class Begin:
-    def __init__(self):
-        print("Gsoc 2024 filteration of projects based on programming languages.")
-        self.lang = input("Enter the name of programming language : ")
-        self.results = []
-
-    def get_input_lang(self):
-        return self.lang
-
-    def set_results(self, value):
-        self.results.append(value)
-
-    def get_results(self):
-        return self.results
+def initialise():
+    set_language()
+    driver = webdriver.Chrome()
+    driver.get("https://summerofcode.withgoogle.com/programs/2024/organizations")
+    time.sleep(10)
+    project_url_collector(driver)
 
 
-driver = webdriver.Chrome()
-driver.get("https://summerofcode.withgoogle.com/programs/2024/organizations")
-root_url = 'https://summerofcode.withgoogle.com/'
-time.sleep(5)
-
-result = []
+all_projects_urls = []
 elements_list = []
 
 
-def process(elements_list_value):
+def process_urls_on_page(elements_list_value):
     for element in elements_list_value:
         all_attributes = element.get_property("attributes")
         for attribute in all_attributes:
             if 'href' in attribute["name"]:
-                result.append(attribute["value"])
+                all_projects_urls.append(attribute["value"])
     del elements_list[:]
+    print("Returning after collecting urls on page")
+    print("Till Now..", all_projects_urls)
+    print("Len = ", len(all_projects_urls))
 
 
-while True:
-    try:
-        wait = WebDriverWait(driver, 10)
-        process(wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@title='Opens in a new window']"))))
-        wait = WebDriverWait(driver, 10)
-        next_page_element = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Next page']")))
-        next_page_element.click()
-        time.sleep(5)
-    except Exception as ex:
-        print("Exception occurred : ", ex)
-        break
-    continue
+def project_url_collector(driver):
+    while True:
+        try:
+            wait = WebDriverWait(driver, 10)
+            process_urls_on_page(
+                wait.until(EC.presence_of_all_elements_located((By.XPATH, "//*[@title='Opens in a new window']"))))
+            wait = WebDriverWait(driver, 10)
+            next_page_element = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Next page']")))
+            next_page_element.click()
+            time.sleep(5)
+        except Exception as ex:
+            print("Exception occurred [project_url_collector]: ", ex)
+            break
+        continue
 
 
-def get_tech(url, Driver, queue):
-    driver = Driver()
+def get_tech(url, driver, queue_res):
+    driver = driver()
     print("WORKING FOR : ", url)
     driver.get(url)
     w = WebDriverWait(driver, 10)
     elem = w.until(EC.presence_of_element_located((By.XPATH, "//*[@class='tech__content']")))
     tech_list_string = elem.text
     if find_match(inputted_language, tech_list_string):
-        queue.put(url)
+        queue_res.put(url)
 
 
-def get_result(obj):
+def get_result():
+    print("\n\n--------------------------- Final Results :----------------------------------")
     while not queue.empty():
         print(queue.get())
-    print("FINAL RESULTS : ", obj.get_results())
 
 
 def clean_url(result_urls):
+    print("CLEANING URLs...", result_urls)
     jobs = []
     print("TOTAL PROJECTS : ", len(result_urls))
-    obj = Begin()
     for url in result_urls:
         if 'http' not in url:
             if url[0] == '/':
@@ -97,18 +94,10 @@ def clean_url(result_urls):
             jobs.append(url)
         else:
             continue
-    run_jobs(jobs)
-    #get_result(obj)
-
-
-print("ALL URLs : ",result)
-clean_url(result)
-driver.close()
-
+    return jobs
 
 
 def process_result_batches(jobs):
-    queue = Queue()
     processes = []
     for i in jobs:
         process = Process(target=get_tech, args=[i, webdriver.Chrome, queue])
@@ -117,23 +106,30 @@ def process_result_batches(jobs):
 
     for process in processes:
         process.join()
-    print("RETURNING.....")
+    print("Done processing this batch.....")
 
 
 def set_language():
     global inputted_language
     inputted_language = input("Enter the name of programming language : ")
 
+
 def run_jobs(jobs):
-    set_language()
     limit = 5
     start = 0
     end = limit
     if len(jobs) < limit:
         process_result_batches(jobs)
     while end < len(jobs):
-        process_result_batches(jobs[start:end+1])
-        start = end+1
-        end = start+limit
+        process_result_batches(jobs[start:end])
+        start = end
+        end = start + limit
 
-run_jobs()
+
+try:
+    initialise()
+except Exception as err:
+    print("Exception occurred during initialisation..", err)
+    sys.exit(1)
+run_jobs(clean_url(all_projects_urls))
+get_result()
